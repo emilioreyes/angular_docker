59d7e04f4fd4bd3fc0dc8551e629926ea2bda26a
darwinemilioreyesreyes <emiliorr2013@gmail.com>
2024-01-15 10:10:53 -0500

    fix v_01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FromDockerComponent } from './dockerModule/from-docker/from-docker.component';
 
 const routes: Routes = [
 
   {
     path: 'home',
     loadChildren: () => import('./homeModule/module.module').then(m => m.ModuleModule)
-  },{
-    path: 'docker',component:FromDockerComponent
   },{
     path: '', pathMatch: 'full', redirectTo: 'home'
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FromDockerComponent } from './dockerModule/from-docker/from-docker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FromDockerComponent,
   ],
   imports: [
     BrowserModule,
